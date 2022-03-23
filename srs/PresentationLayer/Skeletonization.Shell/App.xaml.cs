c5f30b83fbcef4bab669ddfb3f051a0e68efb3fc
GoldGreen <48961363+GoldGreen@users.noreply.github.com>
2022-03-23 12:47:32 +0300

    Init

---
+++ srs/PresentationLayer/Skeletonization.Shell/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Skeletonization.PresentationLayer.Shell
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ srs/PresentationLayer/Skeletonization.Shell/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Skeletonization.PresentationLayer.Shell
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
